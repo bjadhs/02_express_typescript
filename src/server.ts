adfff4f54be120b7c11ccbcb93bf806effed560c
Bijaya Adhikari <bjadhs@gmail.com>
2024-12-29 22:16:30 +1100

    add error handler

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,33 @@
 import {Config} from './config/index';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import createHttpError, {HttpError} from 'http-errors'
 
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send("Hello World");
+app.get('/', async (req, res, next)=>{
+    const err = createHttpError(401,'This is a bad request');
+    next(err);
+    // res.send("Hello World");
+
+
 })
-const startServer =()=>{
+
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    const statusCode = err.statusCode || 500;
+    res.status(statusCode).json({
+        errors: [
+            {
+                type: err.name,
+                msg: err.message,
+                path: '',
+                location: ''
+
+            }
+        ]
+    })
+});
+
+const startServer = () =>{
     const PORT = Config.PORT
     try{
         app.listen(PORT as string, ()=> console.log(`Server is running in port ${PORT}`));
